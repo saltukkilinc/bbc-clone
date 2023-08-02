f06149735f72617204aaa62def306fa8e42b82c7
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-08-02 20:28:38 +0300

    add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,15 +7,44 @@ import SearchIcon from "../../assets/icons/searchIcon";
 
 const Header = () => {
   return (
-    <div className="h-[64px] px-2 bg-[#000] flex flex-col justify-center">
+    <div className="h-[64px] px-2 bg-[#000] flex flex-col justify-center text-white">
       <div className="flex items-center">
-        <UserIcon className="w-8 h-8" />
-        <div className="flex-1 flex justify-center items-center">
+        <div className="hidden sm:block mr-3">
           <BbcIcon />
         </div>
-        <SearchIcon />
-        <div className="ml-3"><HamburgerIcon /></div>
-        <TreeDotsIcon className="hidden md:block"/>
+        <div className="mx-2 flex">
+          <UserIcon className="w-8 h-8" />
+          <div className="hidden px-2 sm:flex cs5:p-0">
+            <div className="hidden cs5:block ml-[6px] w-[65px] flex-1">
+              Sign In
+            </div>
+            <div className="border border-white"></div>
+          </div>
+        </div>
+        <ul className="hidden sm:flex flex-1 justify-around text-white">
+          <li>Home</li>
+          <li>News</li>
+          <li>Sports</li>
+          <li className="hidden cs1:block">Reel</li>
+          <li className="hidden cs2:block">Worklife</li>
+          <li className="hidden cs3:block">Travel</li>
+          <li className="hidden cs4:block">Future</li>
+        </ul>
+        
+        <div className="flex-1 flex justify-center items-center sm:hidden">
+          <BbcIcon />
+        </div>
+
+        <div className="hidden sm:block mr-2">
+          <TreeDotsIcon />
+        </div>
+
+        <div className="sm:hidden mx-3">
+          <HamburgerIcon />
+        </div>
+        <div className="mx-[10px]">
+          <SearchIcon />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': "600px",
+      'md': "768px",
+      'lg': "1024px",
+      'xl': "1280px",
+      '2xl': "1536px",
+    },
+    extend: {
+      screens: {
+        'cs1': "650px",
+        'cs2': "720px",
+        'cs3': "800px",
+        'cs4': "900px",
+        'cs5': "1009px",
+      },
+    },
   },
   plugins: [],
-}
+};
