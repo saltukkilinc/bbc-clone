8054da8260856c57794fd005a2d5608be6ce05f6
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-08-03 11:42:01 +0300

    add hidden navbar

---
+++ src/assets/icons/closeIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+const CloseIcon = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={32}
+    height={32}
+    fill="white"
+    viewBox="0 0 28 28"
+    {...props}
+  >
+    <path
+      d="M7.487 5.71 5.798 7.4l6.545 6.624-6.523 6.6 1.644 1.644 6.514-6.59 6.534 6.613h.002l1.688-1.69-6.545-6.622 6.523-6.6-1.645-1.645-6.513 6.59-6.535-6.612Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default CloseIcon
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,51 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import UserIcon from "../../assets/icons/userIcon";
 import BbcIcon from "../../assets/icons/bbcIcon";
 import TreeDotsIcon from "../../assets/icons/ThreeDotsIcon";
 import HamburgerIcon from "../../assets/icons/hamburgerIcon";
 import SearchIcon from "../../assets/icons/searchIcon";
+import SmallSeacrhIcon from "../../assets/icons/smallSeacrhIcon";
+import CloseIcon from "../../assets/icons/closeIcon";
 
 const Header = () => {
+  const [ expandNavbar, setExpandNavbar ] = useState<boolean>(true);
+
   return (
-    <div className="h-[64px] px-2 bg-[#000] flex flex-col justify-center text-white">
-      <div className="flex items-center">
-        <div className="hidden sm:block mr-3">
-          <BbcIcon />
-        </div>
-        <div className="mx-2 flex">
-          <UserIcon className="w-8 h-8" />
-          <div className="hidden px-2 sm:flex cs5:p-0">
-            <div className="hidden cs5:block ml-[6px] w-[65px] flex-1">
-              Sign In
-            </div>
-            <div className="border border-white"></div>
+    <div className="px-2 bg-[#000] flex flex-col  text-white">
+      <div className="h-[64px]  flex justify-center items-center">
+        <div className="flex flex-1 items-center cs5:max-w-[976px] cs5:justify-center cs6:max-w-[1248px] ">
+          <a
+            className="mx-2 flex items-center sm:border-r sm:border-r-white w-[65px] cs5:w-[153px] hover:underline decoration-white underline-offset-[12px]"
+            href="https://account.bbc.com/signin?realm=%2F&clientId=Account&context=news&ptrt=https%3A%2F%2Fwww.bbc.com%2Fnews&userOrigin=news_gnl&isCasso=false&action=sign-in&redirectUri=https%3A%2F%2Fsession.bbc.com%2Fsession%2Fcallback%3Frealm%3D%2F&service=IdSignInService&nonce=SnEoGcMb-HW50QnOnM_9HUzEqiE_gL_icmtk"
+            target="_blank"
+          >
+            <UserIcon className="w-6 h-6" />
+            <div className="hidden cs5:block ml-[6px]">Sign In</div>
+          </a>
+
+          <ul className="hidden sm:flex flex-1 justify-around text-white">
+            <li className="hover:underline decoration-white underline-offset-[12px]">
+              Home
+            </li>
+            <li className="hover:underline decoration-blue-700 underline-offset-[12px]">
+              News
+            </li>
+            <li className="hover:underline decoration-red-600 underline-offset-[12px]">
+              Sports
+            </li>
+            <li className="hidden cs1:block hover:underline decoration-green-400 underline-offset-[12px]">
+              Reel
+            </li>
+            <li className="hidden cs2:block hover:underline decoration-orange-400 underline-offset-[12px]">
+              Worklife
+            </li>
+            <li className="hidden cs3:block hover:underline decoration-purple-600 underline-offset-[12px]">
+              Travel
+            </li>
+            <li className="hidden cs4:block hover:underline decoration-orange-600 underline-offset-[12px]">
+              Future
+            </li>
+          </ul>
+
+          <div className="flex-1 flex justify-center items-center sm:order-[-1] sm:mr-3 sm:flex-initial">
+            <BbcIcon />
           </div>
-        </div>
-        <ul className="hidden sm:flex flex-1 justify-around text-white">
-          <li>Home</li>
-          <li>News</li>
-          <li>Sports</li>
-          <li className="hidden cs1:block">Reel</li>
-          <li className="hidden cs2:block">Worklife</li>
-          <li className="hidden cs3:block">Travel</li>
-          <li className="hidden cs4:block">Future</li>
-        </ul>
-        
-        <div className="flex-1 flex justify-center items-center sm:hidden">
-          <BbcIcon />
-        </div>
 
-        <div className="hidden sm:block mr-2">
-          <TreeDotsIcon />
-        </div>
+          <div className="hidden sm:block mr-2">
+            <TreeDotsIcon onClick={() => setExpandNavbar(!expandNavbar)} />
+          </div>
 
-        <div className="sm:hidden mx-3">
-          <HamburgerIcon />
-        </div>
-        <div className="mx-[10px]">
-          <SearchIcon />
+          <div className="sm:hidden mx-3">
+            <HamburgerIcon onClick={() => setExpandNavbar(!expandNavbar)}/>
+          </div>
+          <div className="mx-[10px] cs6:hidden">
+            <SearchIcon />
+          </div>
+          <a
+            className="w-[220px]  p-3 hidden cs6:flex items-center bg-[#202224]"
+            href="https://www.bbc.co.uk/search?q=&d=HOMEPAGE_GNL"
+          >
+            <SmallSeacrhIcon />
+            <span className="ml-2">Search BBC</span>
+          </a>
         </div>
       </div>
+      {expandNavbar && (
+        <div className="text-white bg-black flex flex-col justify-center">
+          <div className="flex pt-3">
+            <div className="flex-1">
+              <ul className="grid grid-cols-2">
+                <li className="py-4 hover:border-b border-b-white">Home</li>
+                <li className="py-4">News</li>
+                <li className="py-4">Sports</li>
+                <li className="py-4">Travel</li>
+                <li className="py-4">Worklife</li>
+                <li className="py-4">Travel</li>
+                <li className="py-4">Future</li>
+                <li className="py-4">Culture</li>
+                <li className="py-4">TV</li>
+                <li className="py-4">Weather</li>
+                <li className="py-4">Sounds</li>
+              </ul>
+            </div>
+            <div className="py-4"><CloseIcon /></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
